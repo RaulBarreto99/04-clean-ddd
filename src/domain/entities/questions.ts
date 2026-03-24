bbc4e2c2969c0cd1c0992fef08ccfc0416388942
Raul Barreto <raul_barreto99@live.com>
2026-03-23 22:16:06 -0300

    getters and setters

--- src/domain/entities/questions.ts
+++ src/domain/entities/questions.ts
@@ -1,25 +1,88 @@
-import { randomUUID } from "node:crypto"
-import type { Slug } from "./value-objects/slug.js"
+import { Slug } from "./value-objects/slug.js"
+import { Entity } from "../../core/entities/entity.js"
+import type { UniqueEntityID } from "../../core/entities/unique-entity-id.js"
+import type { Optional } from "../../core/types/optional.js"
+import dayjs from "dayjs"
 
 interface QuestionProps {
+    authorId: UniqueEntityID
+    bestAnswerId?: UniqueEntityID | undefined
     title: string
     content: string
     slug: Slug
-    authorId: string
+    createdAt: Date
+    updatedAt?: Date
 }
 
-export class Question {
-    public id: string
-    public title: string
-    public slug: Slug
-    public content: string
-    public authorId: string
 
-    constructor(props: QuestionProps, id?: string) {
-        this.title = props.title
-        this.content = props.content
-        this.slug = props.slug
-        this.authorId = props.authorId
-        this.id = id ?? randomUUID()
+export class Question extends Entity<QuestionProps> {
+
+    get authorId(){
+        return this.props.authorId
+    }
+
+    get bestAnswerId(){
+        return this.props.bestAnswerId
+    }
+
+    get title(){
+        return this.props.title
+    }
+
+    get content(){
+        return this.props.content
+    }
+
+    get slug(){
+        return this.props.slug
+    }
+
+    get createdAt(){
+        return this.props.createdAt
+    }
+
+    get updatedAt(){
+        return this.props.updatedAt
+    }
+
+    get isNew(): boolean{
+        return dayjs().diff(this.createdAt, 'days') <= 3
+    }
+
+    get excerpt(){
+        return this.content.substring(0, 120).trimEnd().concat('...')
+    }
+
+    private touch(){
+        this.props.updatedAt = new Date()
+    }
+
+    set title(title: string){
+        this.props.title = title
+        this.props.slug = Slug.createFormText(title)
+        this.touch()
+    }
+
+    set content(content: string){
+        this.props.content = content
+
+        this.touch()
+    }
+
+    set bestAnswerId(bestAnswerId: UniqueEntityID | undefined){
+        this.props.bestAnswerId = bestAnswerId
+
+        this.touch()
+    }
+
+    static create(props: Optional<QuestionProps, 'createdAt' | 'slug'>, id?: UniqueEntityID){
+        const question = new Question({
+            ...props,
+            slug: props.slug ?? Slug.createFormText(props.title),
+            createdAt: new Date(),
+
+        }, id)
+
+        return question
     }
 }
