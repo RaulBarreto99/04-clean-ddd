3a209f6f60c9bc2f7b30168a3adcfd3dca96b4b7
RaulBarreto99 <raul_barreto99@live.com>
2026-09-24 11:09:17 -0300

    aggregateroot

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -2,11 +2,13 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Question } from '../../enterprise/entities/question';
 import { QuestionsRepository } from '../repositories/questions-repository';
 import { Either, right } from '@/core/either';
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment';
 
 interface CreateQuestionUseCaseRequest {
     authorId: string
     title: string
     content: string
+    attachmentIds: string[]
 }
 
 type CreateQuestionUseCaseResponse = Either<null, { question: Question }>
@@ -17,14 +19,24 @@ export class CreateQuestionUseCase {
     async execute({
         authorId,
         title,
-        content
+        content,
+        attachmentIds,
     }: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
-
+        
         const question = Question.create({
             authorId: new UniqueEntityID(authorId),
             title,
             content
         })
+        
+        const questionAttachments = attachmentIds.map(attachmentId => {
+            return QuestionAttachment.create({
+                questionId: question.id,
+                attachmentId: new UniqueEntityID(attachmentId),
+            })
+        })
+
+        question.attachments = questionAttachments
 
         await this.questionsRepository.create(question)
 
